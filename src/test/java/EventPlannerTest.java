0fe073a828ab978627dc5416d4a2b49ff80d6693
Paul Guevarra <p.a.guevarra@gmail.com>
2017-08-04 12:23:21 -0700

    create test to receive guest estimated time duration of event

--- src/test/java/EventPlannerTest.java
+++ src/test/java/EventPlannerTest.java
@@ -8,5 +8,9 @@ public class EventPlannerTest {
         EventPlanner testEventPlanner = new EventPlanner();
         assertEquals(25, testEventPlanner.countExpectedGuests(25));
     }
-
+    @Test
+    public void newEventPlanner_getGuestTimeDuration_4(){
+        EventPlanner testEventPlanner = new EventPlanner();
+        assertEquals(4, testEventPlanner.receiveGuestTimeDuration(4));
+    }
 }
