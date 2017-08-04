3fbf802793669d69c2a7612bde8af6b88238fe2f
Paul Guevarra <p.a.guevarra@gmail.com>
2017-08-04 12:28:08 -0700

    create function to receive guest estimated time duration of event

--- src/main/java/EventPlanner.java
+++ src/main/java/EventPlanner.java
@@ -1,8 +1,14 @@
 public class EventPlanner {
     private int expectedNumberGuests;
+    private int expectedDurationTime;
+
 
     public int countExpectedGuests(int userGuestNum){
         expectedNumberGuests = userGuestNum;
         return expectedNumberGuests;
+    }
+    public int receiveGuestTimeDuration(int userEventDuration){
+        expectedDurationTime = userEventDuration;
+        return expectedDurationTime;
     }
 }
