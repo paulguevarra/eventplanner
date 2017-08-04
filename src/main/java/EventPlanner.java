f283a1344fd2fca0055996929df595e87a3683df
Paul Guevarra <p.a.guevarra@gmail.com>
2017-08-04 12:18:32 -0700

    add function to receive estimated guest amount

--- src/main/java/EventPlanner.java
+++ src/main/java/EventPlanner.java
@@ -1,3 +1,8 @@
 public class EventPlanner {
+    private int expectedNumberGuests;
 
+    public int countExpectedGuests(int userGuestNum){
+        expectedNumberGuests = userGuestNum;
+        return expectedNumberGuests;
+    }
 }
